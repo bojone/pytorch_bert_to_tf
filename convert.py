c9973156e651858f886fe9f206e6cef8ce4ea730
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2020-05-19 15:10:10 +0800

    Update convert.py

--- convert.py
+++ convert.py
@@ -5,8 +5,8 @@
 import torch
 import tensorflow as tf
 
-in_file = '/root/kg/bert/thu/baidubaike/pytorch_model.bin'
-out_file = '../baike_bert/bert_model.ckpt'
+in_file = '/root/kg/bert/thu/ms_bert/ms/pytorch_model.bin'
+out_file = '/root/kg/bert/thu/ms_bert/bert_model.ckpt'
 
 torch_weights = torch.load(in_file, map_location='cpu')
 tensors_to_transpose = (
@@ -23,18 +23,22 @@ var_map = (
     ('LayerNorm/weight', 'LayerNorm/gamma'),
     ('LayerNorm/bias', 'LayerNorm/beta'),
     ('weight', 'kernel'),
+    ('cls/predictions/bias', 'cls/predictions/output_bias'),
+    ('cls/seq_relationship/kernel', 'cls/seq_relationship/output_weights'),
+    ('cls/seq_relationship/bias', 'cls/seq_relationship/output_bias'),
 )
 
 
 def to_tf_var_name(name):
     for patt, repl in iter(var_map):
         name = name.replace(patt, repl)
-    return "bert/{}".format(name)
+    return name
 
 
 with tf.Graph().as_default():
     for var_name in torch_weights:
         tf_name = to_tf_var_name(var_name)
+        print(tf_name)
         torch_tensor = torch_weights[var_name].numpy()
         if any([x in var_name for x in tensors_to_transpose]):
             torch_tensor = torch_tensor.T
